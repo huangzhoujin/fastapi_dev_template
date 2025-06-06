e89af8fe9568a29fc060584c76af90400582c634
Huang Zhoujin <huangzhoujin@hotmail.com>
2025-06-06 14:20:58 +0800

    project initial

---
+++ app/apis/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Project :   fastapi_dev_template
+@File    :   __init__.py.py
+@Contact :   zhoujin.huang@genpact.com
+@License :   https://www.genpact.com/privacy/terms-and-conditions
+@Revised              @Author           @Version    @Desciption
+------------------    --------------    --------    -----------
+6/6/2025 2:16 PM       Zhoujin Huang     1.0.0       None
+'''
